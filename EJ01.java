bb05424083d7b974f5ecaf5c5e13e7b695d31ed6
nomanhaseeb <37253791+nomanhaseeb@users.noreply.github.com>
2019-10-29 12:24:43 +0100

    Add files via upload

---
+++ EJ01.java
@@ -0,0 +1,12 @@
+
+public class EJ01 {
+
+	public static void main(String[] args) {
+		// DECLARAR VARIABLES
+		String frase = "me llamo mahamadou";
+		char letra = 'a';
+		System.out.println(frase.replace(letra, Character.toUpperCase(letra)));
+
+	}
+
+}
